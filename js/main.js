c7f0466086a75108a78430be43246446605534a0
Mustafa Çetin <me@owlfurty.com>
2016-11-04 02:46:31 +0100

    productpage

--- js/main.js
+++ js/main.js
@@ -1,10 +1,26 @@
 // sticky header
 
 $(window).scroll(function() {
-if ($(this).scrollTop() > 300){  
+if ($(this).scrollTop() > 1){  
     $('header').addClass("sticky");
   }
   else{
     $('header').removeClass("sticky");
   }
+});
+
+var bLazy = new Blazy({
+    breakpoints: [{
+    width: 420 // Max-width
+      , src: 'data-src-small'
+}]
+  , success: function(element){
+    setTimeout(function(){
+	// We want to remove the loader gif now.
+	// First we find the parent container
+	// then we remove the "loading" class which holds the loader image
+	var parent = element.parentNode;
+	parent.className = parent.className.replace(/\bloading\b/,'');
+    }, 200);
+    }
 });
